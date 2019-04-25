f3bb4de7619ac0280699a48043c496ec5bc450bb
Ethan Sagin <ethansagin@gmail.com>
2019-04-25 22:33:23 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,7 +14,7 @@ class EmailParser
     @email_string = e_string
   end
   
-  def parse(email_string)
+  def parse
     e_arr = email_string.split(", ") || email_string.split(" ")
   binding.pry
   end
